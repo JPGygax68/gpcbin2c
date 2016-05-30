bc83251eb6c4cf4715850dd0523edff0eb40a6d8
Jean-Pierre Gygax <gygax@practicomp.ch>
2016-05-30 22:05:20 +0200

    New feature (not tested yet): create a header/source file pair

--- bin2c.cpp
+++ bin2c.cpp
@@ -2,73 +2,174 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <regex>
+
+struct Params
+{
+    std::string input_file;
+    std::string output_file;
+    std::string const_name;
+    std::string header_file_name;
+    std::string source_file_name;
+
+    void parse(int argc, char *argv[])
+    {
+        using namespace std;
+
+        // Pluck all the parameters
+        // TODO: handle quotation marks
+
+        for (auto i = 1; i < argc; i++)
+        {			
+            string arg(argv[i]);
+
+            if (arg == "-i")                            // short option to specify input filename
+            {
+                if (++i == argc) throw runtime_error("-i must be followed by filename");
+                input_file = argv[i];
+            }
+            else if (arg.substr(0, 8) == "--input=")    // ditto long form
+            {
+                input_file = arg.substr(8);
+            }
+            else if (arg == "-o")                       // short option to specify output filename
+            {
+                if (++i == argc) throw runtime_error("-o must be followed by filename");
+                output_file = argv[i];
+            }
+            else if (arg.substr(0, 9) == "--output=")
+            {
+                output_file = arg.substr(9);
+            }
+            else if (arg == "-c")                       // short option to specify const variable name
+            {
+                if (++i == argc) throw runtime_error("-c must be followed by name of const variable");
+                const_name = argv[i];
+            }
+            else if (arg.substr(0, 8) == "--const=")    // ditto long form
+            {
+                const_name = arg.substr(8);
+            }
+        }
+
+        // TODO: check for leftovers...
+    }
+
+    void check() const
+    {
+        using namespace std;
+        using namespace std::string_literals;
+
+        // Plausibility checks
+
+        if (!output_file.empty() && !const_name.empty())
+            throw runtime_error("Cannot specify both an output file (-o, --output) and a const data name (-c, --const)");
+
+        if (!const_name.empty())
+        {
+            if (!regex_match(const_name, regex{ "[_a-zA-Z][_a-zA-Z0-9]{0,30}" }))
+                throw runtime_error("\""s + const_name + "\" is not a valid C/C++ identifier");
+        }
+    }
+
+    void set_defaults()
+    {
+       if (!const_name.empty())
+       {
+           if (header_file_name.empty()) header_file_name = const_name + ".h";
+           if (source_file_name.empty()) source_file_name = const_name + ".c";
+       }
+    }
+};
+
+static auto convert(std::istream &istream, std::ostream &ostream) -> std::size_t
+{
+    using namespace std;
+
+    size_t n = 0;
+    while (true) {
+        unsigned char byte = static_cast<unsigned int>(istream.get());
+        if (istream.eof()) break;
+        if (n > 0) {
+            ostream << ", ";
+            if (n % 16 == 0) ostream << endl;
+        }
+        ostream << "0x" << hex << setw(2) << setfill('0') << static_cast<unsigned int>(byte);
+        n++;
+    }
+
+    return n;
+}
+
+static void convert_data(std::istream &istream, const std::string output_file)
+{
+    using namespace std;
+
+    ofstream ofs;
+    ostream *os = nullptr;
+
+    if (!output_file.empty()) 
+    {
+        ofs.open(output_file);
+        os = &ofs;
+    }
+    else {
+        os = &cout;
+    }
+
+    convert(istream, *os);
+}
+
+static void generate_files(std::istream &istream, const Params &params)
+{
+    using namespace std;
+
+    ofstream hdr { params.header_file_name };
+    ofstream src { params.source_file_name };
+    
+    src << "const unsigned char " << params.const_name << "[] = {\n";
+    auto size = convert(istream, src);
+    src << "};\n";
+
+    hdr << "extern const unsigned char " << params.const_name << "[" << size << "];\n";
+}
 
 int main(int argc, char *argv[])
 {
 	using namespace std;
 
-	try {
+	try 
+    {
+        Params params;
+        params.parse(argc, argv);
+        params.check();
+        params.set_defaults();
 
-		string input_file, output_file;
+        ios_base::sync_with_stdio(false); // speed-up
 
-		// TODO: handle quotation marks
+        // Open input file / stream
 
-		for (int i = 1; i < argc; i++) {
-			
-			string arg(argv[i]);
+        ifstream ifs;
+        istream *is = nullptr;
+        if (!params.input_file.empty()) 
+        {
+            ifs.open(params.input_file, ios::binary);
+            is = &ifs;
+        }
+        else {
+            is = &cin;
+        }
 
-			if (arg == "-i") {
-				if (++i == argc) throw runtime_error("-i must be followed by filename");
-				input_file = argv[i];
-			}
-			else if (arg.substr(0, 8) == "--input=") {
-				input_file = arg.substr(8);
-			}
-			else if (arg == "-o") {
-				if (++i == argc) throw runtime_error("-o must be followed by filename");
-				output_file = argv[i];
-			}
-			else if (arg.substr(0, 9) == "--output=") {
-				output_file = arg.substr(9);
-			}
-		}
+        // Either convert to output file / stream or generate a pair of files
 
-		ifstream ifs;
-		ofstream ofs;
-		istream *is = nullptr;
-		ostream *os = nullptr;
-
-		if (!input_file.empty()) {
-			ios_base::sync_with_stdio(false);
-			ifs.open(input_file, ios::binary);
-			is = &ifs;
-		}
-		else {
-			is = &cin;
-		}
-
-		if (!output_file.empty()) {
-			ios_base::sync_with_stdio(false);
-			ofs.open(output_file);
-			os = &ofs;
-		}
-		else {
-			os = &cout;
-		}
-
-		// Convert
-
-		size_t n = 0;
-		while (true) {
-			unsigned char byte = static_cast<unsigned int>(is->get());
-			if (is->eof()) break;
-			if (n > 0) {
-				*os << ", ";
-				if (n % 16 == 0) *os << endl;
-			}
-			*os << "0x" << hex << setw(2) << setfill('0') << (unsigned int) byte;
-			n++;
-		}
+        if (params.const_name.empty())
+        {
+            convert_data(*is, params.output_file);
+        }
+        else
+        {
+            generate_files(*is, params);
+        }
 
 		return 0;
 	}
